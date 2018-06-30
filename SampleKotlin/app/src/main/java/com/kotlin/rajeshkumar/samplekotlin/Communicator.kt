84d6d962435ac85211e51f23e4f21db97b665ab2
Rajesh kumar <rajeshkumarchelluri@gmail.com>
2018-06-30 11:41:50 +0530

    initial commit

--- SampleKotlin/app/src/main/java/com/kotlin/rajeshkumar/samplekotlin/Communicator.kt
+++
@@ -1,9 +0,0 @@
-package com.kotlin.rajeshkumar.samplekotlin
-
-/**
- * Created by Rajesh kumar on 17-09-2017.
- */
-
-interface Communicator {
-    fun setData(value: String)
-}
--- SampleKotlin/app/src/main/java/com/kotlin/rajeshkumar/samplekotlin/UserModel.kt
+++
@@ -1,6 +0,0 @@
-package com.kotlin.rajeshkumar.samplekotlin
-
-/**
- * Created by Rajesh kumar on 17-09-2017.
- */
-data class UserModel (val name:String,val address:String)
