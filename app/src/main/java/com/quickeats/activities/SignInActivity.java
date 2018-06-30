e62e81c5e3c804debc5a90531008063e8f45e422
ashok <ashok.e9908@gmail.com>
2018-06-30 14:50:32 +0530

    My commit

---
+++ app/src/main/java/com/quickeats/activities/SignInActivity.java
@@ -0,0 +1,16 @@
+package com.quickeats.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.quickeats.R;
+
+public class SignInActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_singin);
+
+    }
+}
---
+++ app/src/main/java/com/quickeats/activities/SignUpActivity.java
@@ -0,0 +1,16 @@
+package com.quickeats.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.quickeats.R;
+
+public class SignUpActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+
+    }
+}
